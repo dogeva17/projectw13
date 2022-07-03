5bcf4b2cdef4525c77014dd1db42e257911dbdc3
Margarita <mrg90@mail.ru>
2022-07-04 00:01:10 +0300

    Неделя 13

---
+++ index.js
@@ -0,0 +1,44 @@
+
+
+function SayHello()
+{
+let name = prompt('Ваше имя?');
+alert(`Здравствуйте, ${name}!`);
+
+}
+
+
+
+function calcSum()
+{
+let a = +prompt('Введите двузначное число');
+let b = +prompt('Введите двузначное число');
+let c = a + b;
+alert(c);
+}
+
+
+function calcSubstr()
+{
+let a = +prompt('Введите двузначное число');
+let b = +prompt('Введите двузначное число');
+let c = a - b;
+alert(c);
+}
+
+
+function calcMult()
+{
+let a = +prompt('Введите двузначное число');
+let b = +prompt('Введите двузначное число');
+let c = a * b;
+alert(c);
+}
+
+function calcDiv()
+{
+let a = +prompt('Введите двузначное число');
+let b = +prompt('Введите двузначное число');
+let c = a / b;
+alert(c);
+}
